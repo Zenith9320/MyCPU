e57175ca46b3352ae19a77b6eed62b6d5cf6bbbb
FLtheLeatherman <627720734@qq.com>
2026-01-14 18:36:17 +0800

    fix: enum 不可用，record 只能绑定 Bits

--- src/utils.py
+++ src/utils.py
@@ -17,7 +17,7 @@ OP_SYSTEM = Bits(7)(0b1110011)
 
 # ex 阶段
 
-class ALUOp(Enum):
+class ALUOp:
     ADD = Bits(12)(0b000000000001)
     SUB = Bits(12)(0b000000000010)
     SLL = Bits(12)(0b000000000100)
@@ -31,7 +31,7 @@ class ALUOp(Enum):
     SYS = Bits(12)(0b010000000000)
     NOP = Bits(12)(0b100000000000)
 
-class BranchType(Enum):
+class BranchType:
     NONE = Bits(9)(0b000000001)
     BEQ = Bits(9)(0b000000010)
     BNE = Bits(9)(0b000000100)
@@ -42,29 +42,29 @@ class BranchType(Enum):
     JAL = Bits(9)(0b010000000)
     JALR = Bits(9)(0b100000000)
 
-class Op1Type(Enum):
+class Op1Type:
     RS1 = Bits(3)(0b001)
     PC = Bits(3)(0b010)
     ZERO = Bits(3)(0b100)
 
-class Op2Type(Enum):
+class Op2Type:
     RS2 = Bits(3)(0b001)
     IMM = Bits(3)(0b010)
     FOUR = Bits(3)(0b100)
 
 # mem 阶段
 
-class MemOp(Enum):
+class MemOp:
     NONE = Bits(3)(0b001)
     LOAD = Bits(3)(0b010)
     STORE = Bits(3)(0b100)
 
-class MemWidth(Enum):
+class MemWidth:
     BYTE = Bits(3)(0b001)
     HALF = Bits(3)(0b010)
     WORD = Bits(3)(0b100)
 
-class MemSign(Enum):
+class MemSign:
     SIGNED = Bits(2)(0b01)
     UNSIGNED = Bits(2)(0b10)
 
@@ -74,35 +74,35 @@ WbCtrlSignals = Record(
 )
 
 MemCtrlSignals = Record(
-    mem_op = MemOp,
-    mem_width = MemWidth,
-    mem_sign = MemSign,
+    mem_op = Bits(3),
+    mem_width = Bits(3),
+    mem_sign = Bits(2),
     rd = Bits(5),
     is_halt = Bits(1),
 )
 
 ExCtrlSignals = Record(
-    alu_op = ALUOp,
-    branch_type = BranchType,
-    op1_type = Op1Type,
-    op2_type = Op2Type,
+    alu_op = Bits(12),
+    branch_type = Bits(9),
+    op1_type = Bits(3),
+    op2_type = Bits(3),
     predicted_pc = Bits(32),
-    mem_op = MemOp,
-    mem_width = MemWidth,
-    mem_sign = MemSign,
+    mem_op = Bits(3),
+    mem_width = Bits(3),
+    mem_sign = Bits(2),
     rd = Bits(5),
     is_halt = Bits(1),
 )
 
 # bypass 阶段
 
-class Rs1Type(Enum):
+class Rs1Type:
     NONE = Bits(4)(0b0001)
     EX = Bits(4)(0b0010)
     MEM = Bits(4)(0b0100)
     WB = Bits(4)(0b1000)
 
-class Rs2Type(Enum):
+class Rs2Type:
     NONE = Bits(4)(0b0001)
     EX = Bits(4)(0b0010)
     MEM = Bits(4)(0b0100)

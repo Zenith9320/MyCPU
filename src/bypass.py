a55c98d5aa5e36ab762eff3275d53dcc998999ca
FLtheLeatherman <627720734@qq.com>
2026-01-15 23:57:06 +0800

    fix: bypass need mem_is_store

--- src/bypass.py
+++ src/bypass.py
@@ -14,6 +14,7 @@ class Bypass(Downstream):
         ex_is_load: Value,
         ex_is_store: Value,
         mem_dest_addr: Value,
+        mem_is_store: Value,
         wb_dest_addr: Value,
     ):
         rs1_addr_val = rs1_addr.optional(Bits(5)(0))
@@ -22,6 +23,7 @@ class Bypass(Downstream):
         ex_is_load_val = ex_is_load.optional(Bits(1)(0))
         ex_is_store_val = ex_is_store.optional(Bits(1)(0))
         mem_dest_addr_val = mem_dest_addr.optional(Bits(5)(0))
+        mem_is_store_val = mem_is_store.optional(Bits(1)(0))
         wb_dest_addr_val = wb_dest_addr.optional(Bits(5)(0))
 
         log(f"Bypass: rs1={rs1_addr_val}, rs2={rs2_addr_val}, ex_dest={ex_dest_addr_val}, ex_is_load={ex_is_load_val}, ex_is_store={ex_is_store_val}, mem_dest={mem_dest_addr_val}, wb_dest={wb_dest_addr_val}")
@@ -30,9 +32,10 @@ class Bypass(Downstream):
         rs2_is_zero = rs2_addr_val == Bits(5)(0)
 
         # 可能的 stall 情况
-        # ex load, ex store, 下个周期会占用 memory, 故而要 stall
+        # ex load, ex store, 本周期会占用 memory, 故而要 stall
+        # mem store 本周期也要占用 memory, 也要 stall
 
-        is_stall = ex_is_store_val | ex_is_load_val
+        is_stall = ex_is_store_val | ex_is_load_val | mem_is_store_val
 
         rs1_wb_type = ((rs1_addr_val == wb_dest_addr_val) & (~rs1_is_zero)).select(Rs1Type.WB, Rs1Type.NONE)
         rs1_mem_type = ((rs1_addr_val == mem_dest_addr_val) & (~rs1_is_zero)).select(Rs1Type.MEM, rs1_wb_type)

986262c1bc4bdb3d319d5954cdfeafbeffd2e537
FLtheLeatherman <627720734@qq.com>
2026-01-15 23:55:36 +0800

    feat: memory access

--- src/MA.py
+++ src/MA.py
@@ -1,5 +1,5 @@
 from assassyn.frontend import *
-from utils import MemOp, MemWidth, MemSign, MemCtrlSignals
+from utils import *
 
 class MemoryAcess(Module):
     def __init__(self):
@@ -21,31 +21,35 @@ class MemoryAcess(Module):
         mem_width = ctrl.mem_width
         mem_sign = ctrl.mem_sign
 
-        with Condition(mem_op == MemOp.NONE):
-            log("Memory Access: NONE")
-        with Condition(mem_op == MemOp.LOAD):
-            log("Memory Access: LOAD")
-        with Condition(mem_op == MemOp.STORE):
-            log("Memory Access: STORE")
-        
-        with Condition(mem_width == MemWidth.BYTE):
-            log(" - Width: BYTE")
-        with Condition(mem_width == MemWidth.HALF):
-            log(" - Width: HALF")
-        with Condition(mem_width == MemWidth.WORD):
-            log(" - Width: WORD")
-
-        with Condition(mem_sign == MemSign.SIGNED):
-            log(" - Sign: SIGNED")
-        with Condition(mem_sign == MemSign.UNSIGNED):
-            log(" - Sign: UNSIGNED")
-
         raw_data = sram_dout[0].bitcast(Bits(32))
 
         half_sel = alu_result[1:1].select(raw_data[16:31], raw_data[0:15])
+        byte_sel = alu_result[0:0].select(half_sel[8:15], half_sel[0:7])
 
+        pad_bit_8 = mem_sign.select(Bits(1)(0), byte_sel[7:7])
+        pad_bit_16 = mem_sign.select(Bits(1)(0), half_sel[15:15])
+        
+        padding_8 = pad_bit_8.select(Bits(24)(0xFFFFFF), Bits(24)(0x00000000))
+        padding_16 = pad_bit_16.select(Bits(16)(0xFFFF), Bits(16)(0x0000))
 
+        byte_extended = concat(padding_8, byte_sel)
+        half_extended = concat(padding_16, half_sel)
 
-        write_back.async_called(index = Bits(5)(0), data = Bits(32)(0))
+        load_res = mem_width.select1hot(
+            byte_extended,
+            half_extended,
+            raw_data
+        )
 
-        return rd, data
+        is_load = mem_op == MemOp.LOAD
+        is_store = mem_op == MemOp.STORE
+        final_data = is_load.select(load_res, alu_result)
+
+        wb_ctrl = WbCtrlSignals.bundle(
+            rd = ctrl.rd,
+            is_halt = ctrl.is_halt,
+        )
+
+        write_back.async_called(ctrl = wb_ctrl, data = final_data)
+
+        return ctrl.rd, final_data, is_store
